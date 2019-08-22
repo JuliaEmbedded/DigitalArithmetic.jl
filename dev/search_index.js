ac4c40a0a22c47c131b7521570f3651cd3d6bab6
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-08-22 14:37:29 +0000

    build based on a2cf9c1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DigitalArithmetic.jl-1","page":"Home","title":"DigitalArithmetic.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [DigitalArithmetic]","category":"page"}]
+}
